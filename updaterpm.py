c8c424137df3471f25fd9b3fbd54ecce90683bbe
mark25@UMBC.EDU <mark25@UMBC.EDU@bb2173db-233b-63cc-8cb0-ec26fdc3f785>
2013-05-16 00:35:43 +0000

    Added code to verify dependencies

---
+++ updaterpm.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import os
+import shutil
+import yum
+import sys
+import re
+from types import *
+
+yumopts = ['yum', '-y', '--nogpgcheck']
+
+# Clean Yum cache files to make sure that we get the most recent ones
+os.system('yum clean all')
+
+base = yum.YumBase()
+base.doGenericSetup()
+
+# Gets all installed groups, and the available but not installed groups
+installedGroups, availableGroups = base.doGroupLists()
+installedGroups = set([i.name for i in installedGroups])
+availableGroups = set([i.name for i in availableGroups])
+
+
+# Packages that are installed, but not in any repository
+available = base.doPackageLists('available').available
+
+# For any package for which there is an update, the older version is marked as
+# an extra, but we don't want to remove them, so we get a list of them:
+installed = base.doPackageLists('installed').installed
+available = set(installed + available)
+available = [x.name for x in available]	
+availableGroups = [(base.comps.return_groups(x)[0].name, base.comps.return_groups(x)[0].packages) for x in availableGroups]
+for i in availableGroups:
+  if [x for x in i[1] if not(x in available)]:
+    print "\n" + i[0] + ":"
+    for j in set(["\t"+x+"\n" for x in i[1] if not(x in available)]):
+      print j,
