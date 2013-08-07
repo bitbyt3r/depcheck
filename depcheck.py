3219eba44d2e8d1832ba9a264a6ea9a69f25816f
mark25@UMBC.EDU <mark25@UMBC.EDU@bb2173db-233b-63cc-8cb0-ec26fdc3f785>
2013-08-07 14:21:54 +0000

    Added code to depcheck to prevent showing unused groups

--- depcheck.py
+++ depcheck.py
@@ -5,7 +5,9 @@ import yum
 import sys
 import re
 from types import *
+import ConfigParser
 
+CONFIGFILE = "/usr/csee/etc/updaterpm.conf"
 yumopts = ['yum', '-y', '--nogpgcheck']
 
 # Clean Yum cache files to make sure that we get the most recent ones
@@ -19,19 +21,24 @@ installedGroups, availableGroups = base.doGroupLists()
 installedGroups = set([i.name for i in installedGroups])
 availableGroups = set([i.name for i in availableGroups])
 
+with open(CONFIGFILE) as configFile:
+  config = ConfigParser.ConfigParser()
+  config.readfp(configFile)
+  allowedGroups = set([i.strip().split("@")[-1] for i in config.get("main", "allowed").split("\n")])
+  ignoreGroups = set([i.strip().split("@")[-1] for i in config.get("main", "ignore").split("\n")])  
 
 # Packages that are installed, but not in any repository
-available = base.doPackageLists('available').available
+available = base.doPackageLists('available').available  
 
 # For any package for which there is an update, the older version is marked as
 # an extra, but we don't want to remove them, so we get a list of them:
 installed = base.doPackageLists('installed').installed
 available = set(installed + available)
-available = [x.name for x in available]	
+available = [x.name for x in available]
 availableGroups = [(base.comps.return_groups(x)[0].name, base.comps.return_groups(x)[0].packages) for x in availableGroups]
 whole = []
 for i in availableGroups:
-  if [x for x in i[1] if not(x in available)]:
+  if [x for x in i[1] if not(x in available)] and i in allowedGroups:
     print "\n" + i[0] + ":"
     gaps = list(set(["\t"+x+"\n" for x in i[1] if not(x in available)]))
     gaps.sort()
